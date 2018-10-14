5afff634c4adf367a3c5d8e6863ce277aa7d58bc
Diego Cruz <diegodemcruz@gmail.com>
2018-10-13 22:43:08 -0300

    Acrescentado remover final, remover inicio / remover meio

--- src/lista_Lineares/Lista.java
+++ src/lista_Lineares/Lista.java
@@ -44,5 +44,66 @@ public class Lista {
 		}
 		return lista;
 	}
+	
+	public void remover_inicio() {
+		if (cabeca_lista != null ) {
+			cabeca_lista = cabeca_lista.prox;
+		}
+	}
+
+	public void remover_final() {
+		No aux1 = cabeca_lista;
+		No aux2 = cabeca_lista;
+		if (aux1 == null) {
+			System.out.println("Lista vazia");
+		} else if (aux1.prox == null) {
+			cabeca_lista = null; // a lista só tem um elemento
+
+		} else {
+			while (aux1.prox != null) {
+				aux1 = aux1.prox; // posiciona aux1 na última posição
+			}
+			while (aux2.prox != aux1) {
+				aux2 = aux2.prox; // posiciona aux2 na penultima posição
+			}
+			aux2.prox = null; // exclui o último elemento
+		}
+	}
+
+	public int getQtdElementos() {
+		int qtd = 0;
+		No aux = cabeca_lista;
+		while (aux != null) {
+			qtd = qtd++;
+			aux = aux.prox;
+		}
+		return qtd;
+	}
+	
+	public String remover_posicao (int posicao) {
+		No aux  = cabeca_lista;
+		String msg =" ";
+		int qtd_elementos = getQtdElementos();
+		
+		if (cabeca_lista == null) {
+			msg = "Lista vazia";
+		}else if (qtd_elementos < posicao) {
+			msg ="Menos Elementos do que a posicao requerida";
+		}else if (posicao == 1) {
+			remover_inicio();
+			msg  = " Removido Inicio";
+		}else if (posicao == qtd_elementos) {
+			remover_final();
+			msg = " Removido Final";
+		}else {
+			for (int i=1; i<posicao; i++) {
+				aux = aux.prox;
+			}
+			No aux2 = aux.prox;
+			aux.prox = aux2.prox;
+			msg = "Removido Meio";
+		}
+		return msg;
+	}
 
 }
